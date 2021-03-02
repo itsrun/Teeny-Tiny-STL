4e1375a7d78a4c6733f7f8c73ca5f375a04146cc
LumosHR <19307110301@fudan.edu.cn>
2021-03-03 00:47:25 +0800

    update

--- LMSTL_test.cpp
+++ LMSTL_test.cpp
@@ -1,15 +1,28 @@
 #include "vector_test.h"
 #include "list_test.h"
 #include "map_test.h"
+#include "algo.h"
 
 using namespace lmstl;
 
 int main() {
-	
+	/*
 	cout << CYAN;
 	map_test();
 	vector_test();
 	list_test();
+	*/
+	vector<int> v;
+	for (int i = 0; i < 20; ++i)
+		v.emplace_back(rand());
+	auto p = v.begin() + 9;
+	SHOW(v);
+	cout << *p << endl;
+	nth_element(v.begin(), p, v.end());
+	SHOW(v);
+	cout << *p << endl;
+	sort(v.begin(), v.end());
+	SHOW(v);
 	system("pause");
 	return 0;
 }
